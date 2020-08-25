97c72057ecced4a72ad4dac670dea22e050bb587
twry0617 <twsoccer15@icloud.com>
2020-08-26 08:37:04 +0900

    [add] ToDoアプリのタスク作成機能

--- sample/app/Http/Controllers/TaskController.php
+++ sample/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 use App\Folder;
 use App\Task;
 
@@ -23,4 +24,28 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    public function showCreateForm(int $id){
+
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+{
+    $current_folder = Folder::find($id);
+
+    $task = new Task();
+    $task->title = $request->title;
+    $task->due_date = $request->due_date;
+
+    $current_folder->tasks()->save($task);
+
+    return redirect()->route('tasks.index', [
+        'id' => $current_folder->id,
+    ]);
+}
+
+
 }
--- sample/database/seeds/FoldersTableSeeder.php
+++ sample/database/seeds/FoldersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
 
 class FoldersTableSeeder extends Seeder
 {
@@ -16,6 +19,8 @@ class FoldersTableSeeder extends Seeder
         foreach($titles as $title){
             DB::table('folders')->insert([
                 'title' =>$title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ]);
         }
     }
